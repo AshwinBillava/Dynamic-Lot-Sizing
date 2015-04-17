877ad3a12fee1e07f0f06480483c0b129c43d0fb
AshwinBillava <ashwin.uconn@gmail.com>
2015-04-17 17:15:16 -0400

    added comments & clean-up

--- main.m
+++ main.m
@@ -1,8 +1,8 @@
-close all;
-clear all;
-clc;
+close all
+clear all
+clc
 
-% generate demand
+%%% generate demand
 % n = 10;
 % t = 1:n;
 t = [3 4 6 8 9 10 14 15 19 20];
@@ -11,51 +11,46 @@ t = [3 4 6 8 9 10 14 15 19 20];
 demand = [8 6 8 4 6 7 8 5 9 7];
 demand_cum = cumsum(demand);
 
-% plot cummulative demand
+%%% plot cummulative demand
 stairs([0 t t(end)+1],[0 demand_cum demand_cum(end)])
 hold on
 
-% set production rate
+%%% set production rate
 % q = sum(demand_cum(end))/n
 q = 5;
 
-% minimum q for All-At-Once
+%%% minimum q for All-At-Once
 %[q_min,q_min_ind] = nanmin((ones(1,n)*demand_cum(end)-demand_cum)./(ones(1,n)*n-t))
 %plot([(n-(demand_cum(end)/q_min)),n],[0,(n-(n-(demand_cum(end)/q_min)))*q_min],'r')
 
-% elemintate zero demand events
-disp('Time & Cum Demand after zero demand events')
-demand = [demand_cum(1) diff(demand_cum)];
+%%% elemintate zero demand events
+% disp('Time & Cum Demand after zero demand events')
+demand = [demand_cum(1) diff(demand_cum)]; % re-create demand if not specified
 t_ind = find(demand>0);
-t = t(t_ind);
-demand_cum = demand_cum(t_ind);
+t = t(t_ind); % time array after zero demand event eleminated
+demand_cum = demand_cum(t_ind); % cumulative demand after zero demand event eleminated
 
-t_prime = t - demand_cum/q;
-% find production restriction rate
+%%% find production restriction rate
 ind = 0; %the time of the current dominated event
 kk = 0;
 dem_ind = [];
 dem_time = [];
 while ind < t(end)
-    disp('find min of following')
     time = t(kk+1:end);
     dem_c = demand_cum(kk+1:end);
     [min,ind] = prr(time,dem_c,q); %minimum time t' and the time of corresponding event
-    ind %not a valid statement
     kk = kk + find(time==ind); %what indix belongs to the time ind
     dem_ind = [dem_ind ind];
     dem_time = [dem_time min];
 end
 
-
-
-%plot production rate curves after PRR elemination
+%%% plot production rate curves after PRR elemination
 for j = 1:length(dem_time)
     %plot([dem_time(j),t(end)],[0,(t(end)-dem_time(j))*q],'g')
     plot([dem_time(j),dem_ind(j)],[0,(dem_ind(j)-dem_time(j))*q],'g')
 end
 
-%plot updated cumulative demand curves
+%%% plot updated cumulative demand curves
 [lia,locb] = ismember(dem_ind,t);
 demand_cum_c = demand_cum(locb);
 
